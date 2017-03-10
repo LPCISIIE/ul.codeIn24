3a5ccb07ca6a69ffce7f991ff8ea38ecb4a33977
Alexis Wurth <alexis.wurth57@gmail.com>
2017-03-10 16:27:50 +0100

    Create tables

--- Server/bootstrap/database/app.php
+++ Server/bootstrap/database/app.php
@@ -9,3 +9,34 @@ Manager::schema()->create('account', function (Blueprint $table) {
     $table->string('token')->unique();
     $table->timestamps();
 });
+
+Manager::schema()->create('music', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('title');
+    $table->string('artist')->nullable();
+    $table->string('genre')->nullable();
+    $table->string('length')->nullable();
+    $table->string('url');
+    $table->timestamps();
+});
+
+Manager::schema()->create('room', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('name');
+    $table->unsignedInteger('music_id');
+    $table->foreign('music_id')->references('id')->on('music');
+});
+
+Manager::schema()->create('account_room', function (Blueprint $table) {
+    $table->unsignedInteger('account_id');
+    $table->unsignedInteger('room_id');
+    $table->boolean('dj');
+    $table->foreign('account_id')->references('id')->on('account');
+    $table->foreign('room_id')->references('id')->on('room');
+});
+
+Manager::schema()->create('account_music_room', function (Blueprint $table) {
+    $table->unsignedInteger('account_id');
+    $table->unsignedInteger('music_id');
+    $table->unsignedInteger('room_id');
+});
