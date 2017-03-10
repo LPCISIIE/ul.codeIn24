bfffc1b2c52742a4cb85ff69881e4dbd30381124
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-03-10 17:25:21 +0100

    route de recherche

--- Client/src/routes.js
+++ Client/src/routes.js
@@ -4,6 +4,7 @@ import GuestMiddleware from 'middleware/guest.middleware'
 import HomeController from 'app/home/home.controller'
 import LoginController from 'app/authentication/login.controller'
 import RegisterController from 'app/authentication/register.controller'
+import SearchController from 'app/jamendo-search/search.controller'
 
 export default function route ($stateProvider) {
   $stateProvider
@@ -26,5 +27,11 @@ export default function route ($stateProvider) {
       controller: RegisterController,
       controllerAs: 'Register',
       onEnter: GuestMiddleware
+    })
+    .state('search', {
+      url: '/search',
+      template: require('app/jamendo-search/search.html'),
+      controller: SearchController,
+      controllerAs: 'Search'
     })
 }
