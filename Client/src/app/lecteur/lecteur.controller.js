ecf676349400cd7b753bee948cc0a00d5e38bef9
iryu54 <corentinlabroche@gmail.com>
2017-03-10 21:29:35 +0000

    Position du lecteur par argument

--- Client/src/app/lecteur/lecteur.controller.js
+++ Client/src/app/lecteur/lecteur.controller.js
@@ -4,49 +4,57 @@ export default class LecteurController {
   constructor ($scope) {
     this.$scope = $scope
     let self = this
-    this.miniLecteur = $('#miniLecteur')
-    this.musique = $('#musique')
-    this.playButton = $('#buttonPlay')
-    this.muteButton = $('#buttonMute')
-    this.progress = $('#progress')
-    this.progressBar = $('#progressBar')
-    this.volume = $('#volume')
-    this.volumeBar = $('#volumeBar')
-    this.control = $('#control')
-    this.miniInfo = $('#miniInfo')
-    this.picture = $('#picture')
-    this.volumeSlider = $('#volumeSlider')
+    let elem = {
+      miniLecteur: $('#miniLecteur'),
+      musique: $('#musique'),
+      playButton: $('#buttonPlay'),
+      muteButton: $('#buttonMute'),
+      progress: $('#progress'),
+      progressBar: $('#progressBar'),
+      volume: $('#volume'),
+      volumeBar: $('#volumeBar'),
+      control: $('#control'),
+      miniInfo: $('#miniInfo'),
+      picture: $('#picture'),
+      volumeSlider: $('#volumeSlider')
+    }
 
-    this.volume.on('move', function (event) {
+    elem.volume.on('move', function (event) {
       self.changeVolume(self.seek(event, this))
-      if (self.seek(event, this) === 0) {
-        this.muteButton.addClass('mute')
-      } else {
-        this.muteButton.addClass('mute')
-      }
     })
-    this.volume.on('click', function (event) {
+    elem.volume.on('click', function (event) {
       let ratio = self.seek(event, this)
       self.changeVolume(ratio)
-      if (self.seek(event, this.volume) === 0) {
-        this.muteButton.addClass('mute')
-      } else {
-        this.muteButton.addClass('mute')
-      }
     })
-    this.changeVolume = function (ratio) {
-      this.volumeBar.css('width', ratio * 100 + '%')
-      this.musique[0].volume = ratio
-    }
 
-    this.changeProgression = function (ratio) {
-      var currentTime = ratio * this.musique[0].duration
-      if (currentTime < this.musique[0].duration) {
-        this.musique[0].currentTime = currentTime
+    self.changeVolume = function (ratio) {
+      if (ratio === 0) {
+        elem.muteButton.addClass('mute')
+      } else {
+        elem.muteButton.removeClass('mute')
       }
+      elem.volumeBar.css('width', ratio * 100 + '%')
+      elem.musique[0].volume = ratio
     }
 
-    this.seek = function (e, objet) {
+    self.timer = () => {
+      let percent = (elem.musique[0].currentTime * 100) / elem.musique[0].duration
+      percent += '%'
+      elem.progressBar.width(percent)
+    }
+
+    self.play = (mp3) => {
+      elem.musique.attr('src', mp3.url)
+      setTimeout(function () {
+        var currentTime = mp3.ratio * elem.musique[0].duration
+        if (currentTime < elem.musique[0].duration) {
+          elem.musique[0].currentTime = currentTime
+        }
+        elem.musique[0].play()
+      }, 10)
+    }
+
+    self.seek = function (e, objet) {
       var width = e.pageX - objet.offsetLeft
       var ratio = (width / objet.offsetWidth)
       if (ratio < 0) {
@@ -57,10 +65,6 @@ export default class LecteurController {
       console.log(ratio)
       return ratio
     }
-    this.progress.on('move', (event) => {
-      self.changeProgression(self.seek(event, this.progress))
-    })
-    this.progress.on('click', function (event) { self.changeProgression(self.seek(event, this)) })
 
     $scope.artist = 1
     $scope.playlist = {a: 'lknkln'}
@@ -68,9 +72,11 @@ export default class LecteurController {
       $scope.artist = mp3.artist
       $scope.album = mp3.album
       this.url = mp3.url
-      this.musique.attr('src', mp3.url)
-      this.musique[0].play()
+      self.play(mp3)
     }
+    setInterval(() => {
+      self.timer()
+    }, 200)
   }
 }
 
